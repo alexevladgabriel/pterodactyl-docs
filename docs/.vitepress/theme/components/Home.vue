010d6693d7e26b7ef7cb414866ad67d42c0428e5
Scai <59282365+alexevladgabriel@users.noreply.github.com>
2024-01-24 20:03:26 +0200

    wip

--- docs/.vitepress/theme/components/Home.vue
+++ docs/.vitepress/theme/components/Home.vue
@@ -1,52 +1,51 @@
 <script setup>
-import { VPButton } from 'vitepress/theme';
+import Button from './Button.vue';
+import { GithubIcon } from 'lucide-vue-next';
 </script>
 
 <template>
-    <section id="hero">
-        <div
-            class="absolute inset-0 bottom-10 bg-bottom bg-no-repeat bg-gradient-to-b from-[#10529f] to-slate-300 dark:to-zinc-900"
-        >
-            <div
-                id="test"
-                class="absolute inset-0 bg-grid-slate-900/[0.04] bg-[bottom_1px_center] dark:bg-grid-slate-400/[0.05] dark:bg-bottom dark:border-b dark:border-slate-100/5"
-            ></div>
-        </div>
-        <div class="flex flex-col relative z-10 items-center">
-            <img class="h-48" src="/logos/pterry.svg" alt="Pterodactyl" />
-            <h1 class="tagline font-semibold mb-4">Pterodactyl</h1>
-            <VPButton text="Let's Get Started!" href="/panel/1.0/getting_started" />
+    <section id="hero" class="max-w-[var(--vp-layout-max-width)] px-8 mx-auto mt-8 md:mt-12 xl:mt-24">
+        <div class="grid grid-cols-1 lg:grid-cols-2 space-y-12 lg:space-y-0">
+            <div class="space-y-10 flex flex-col items-center text-center lg:text-left lg:items-start">
+                <div class="md:max-w-lg">
+                    <span class="text-gray-400 text-sm lg:text-base">The #1 software for your game servers</span>
+                    <div class="space-y-8 mt-6">
+                        <h1 class="text-3xl md:text-6xl font-bold">Get total control over your game servers now</h1>
+                        <p>Free, open-source game server management panel</p>
+                    </div>
+                </div>
+                <div class="space-x-3 flex">
+                    <Button
+                        class="!rounded-md !flex items-center"
+                        theme="brand"
+                        size="big"
+                        href="/panel/1.0/getting_started"
+                    >
+                        <template #text>Get started</template>
+                    </Button>
+                    <Button
+                        class="!rounded-md !flex items-center"
+                        theme="alt"
+                        size="big"
+                        href="https://github.com/pterodactyl/panel"
+                    >
+                        <template #icon>
+                            <GithubIcon class="mr-2" />
+                        </template>
+                        <template #text>GitHub</template>
+                    </Button>
+                </div>
+            </div>
+            <div class="bg-emerald-700 rounded-lg">
+                <iframe
+                    class="w-full h-full rounded-lg"
+                    src="https://www.youtube.com/embed/_ypAmCcIlBE?si=7V1cOXzrOAFG2KI3"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                ></iframe>
+            </div>
         </div>
     </section>
 </template>
-
-<style scoped>
-#hero {
-    padding: 96px 32px;
-    text-align: center;
-}
-
-.tagline {
-    background: -webkit-linear-gradient(317deg, #10529f 25%, #1f65b4);
-    font-size: 45px;
-    line-height: 1.25;
-    letter-spacing: -1.5px;
-    margin-top: 2rem;
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-}
-
-html:not(.dark) .accent,
-.dark .tagline {
-    background-image: -webkit-linear-gradient(317deg, #1f65b4 25%, #10529f 75%);
-}
-
-:is(.dark .dark\:bg-grid-slate-400\/\[0\.05\]) {
-    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='rgb(148 163 184 / 0.05)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e");
-}
-
-.bg-grid-slate-900\/\[0\.04\] {
-    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='rgb(15 23 42 / 0.04)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e");
-}
-</style>
